a46a187ed43f1c4c1e28d05414e000ce2e8aeffe
gw2083 <gw2083@naver.com>
2022-11-01 16:42:35 +0900

    post

--- Node_05/README.md
+++ Node_05/README.md
@@ -10,5 +10,10 @@
 ## express View Templage 설정하기
 
 - NodeJS + Express 프로젝트에서 자체 View 화면 만들기
-- ejs(Extends JavaScript View) : html 과 JS 가 결합된 특별한 화면 구현 도구
+- ejs(Embeded JavaScript View) : html 과 JS 가 결합된 특별한 화면 구현 도구
 - ejs templage 컴파일 도구 : `npm install -save ejs`
+- ejs 파일 만들기 : `project/views` 폴더에 \*.ejs 이름으로 파일 생성
+- ejs 파일은 기본구조가 html5 와 같다
+- html5 에서 사용하는 모든 tag, css, js 까지 모두 그대로 사용할 수 있다
+- 기본적인 html5 문서에 JS 코드를 삽입하여 html 과 데이터를 rendering 하는 형식의 코드를 만든다
+- router 에서는 데이터가 준비되면 res.render() 함수를 사용하여 ejs 파일과 JSON 객체 데이터를 묶어서 rendering 한 후 client 로 reponse 한다
--- Node_05/routes/country.js
+++ Node_05/routes/country.js
@@ -6,24 +6,47 @@ const router = express.Router();
 router.get("/", (req, res) => {
   const countrySelect = "SELECT * FROM country ORDER BY name";
   mysql.query(countrySelect, (err, result, fields) => {
-    res.send(result);
+    /**
+     * 국가 = {code, name, ..}
+     * result = [{국가},{국가},{국가}, ..]
+     */
+    //res.send(result);
+    res.render("country", { countrys: result });
+  });
+});
+
+// 주소창에 http://localhost:3000/country/list 입력하고 Enter 눌렀을 때 처리하는 URI
+// 또는 메뉴에서 link 를 클릭했을 때 처리하는 URI
+// 화면을 보여줌
+router.get("/list", (req, res) => {
+  res.render("country", { countrys: [] });
+});
+
+// 데이터를 처리함
+router.post("/list", (req, res) => {
+  // form 의 input 에 설정된 name(c_name) 변수값을 setter 하여 name 변수에 저장
+  const name = req.body.c_name;
+  console.log(name);
+  const sql = "SELECT * FROM country WHERE name LIKE " + " CONCAT('%',?,'%')";
+  mysql.execute(sql, [name], (err, countrys, fields) => {
+    res.render("country", { countrys });
   });
 });
 
 router.get("/:name/get", (req, res) => {
   const name = req.params.name;
   const countrySelectName = "SELECT * FROM country WHERE name =? ORDER BY code";
-  mysql.query(countrySelectName, [name], (err, result, fields) => {
-    res.json(result);
+  mysql.execute(countrySelectName, [name], (err, countrys, fields) => {
+    res.render("country", { countrys });
   });
 });
 
 router.get("/:name/like", (req, res) => {
   const name = req.params.name;
   const countrySelectName =
-    "SELECT * FROM country WHERE name LIKE CONCAT('%','?','%') ORDER BY name";
-  mysql.query(countrySelectName, [name], (err, result, fields) => {
-    res.json(result);
+    "SELECT * FROM country WHERE name LIKE CONCAT('%',?,'%') ORDER BY name";
+  mysql.execute(countrySelectName, [name], (err, countrys, fields) => {
+    res.render("country", { countrys });
   });
 });
 
